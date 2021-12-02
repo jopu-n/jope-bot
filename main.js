91571822eb6d104aa0a57866d03d15c909f69229
Natunen Johannes <75314584+jopu-n@users.noreply.github.com>
2021-12-02 23:47:28 +0200

    New commands yay

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const client = new Client( { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] } );
 const {token} = require("./config.json");
 const fs = require("fs");
+const music = require('@koenie06/discord.js-music');
 
 client.commands = new Discord.Collection();
 
@@ -20,6 +21,14 @@ client.once("ready", () => {
     console.log('Ilpo online')
 } );
 
+music.event.on('playSong', (channel, songInfo) => {
+	channel.send({ content: `:sunglasses: DJ Ilpo tässä! Nyt soi ***${songInfo.title}***` });
+});
+
+music.event.on('addSong', (channel, songInfo) => {
+	channel.send({ content: `:sunglasses: ***${songInfo.title}*** Lisätty DJ Ilpon biisijonoon.` });
+});
+
 client.on("message", message => {
 
     if(!message.content.startsWith(PREFIX) || message.author.bot) return;
@@ -30,11 +39,17 @@ client.on("message", message => {
     if(command == "ilpo"){
         client.commands.get("ilpo").execute(message, args);
     }
+    else if(command == "playOld" || command == "pOld"){
+        client.commands.get("playOld").execute(message, args);
+    }
+    else if(command == "stop"){
+        client.commands.get("stop").execute(message);
+    }
     else if(command == "play" || command == "p"){
         client.commands.get("play").execute(message, args);
     }
-    else if(command == "stop" || command == "s"){
-        client.commands.get("stop").execute(message, args);
+    else if(command == "skip" || command == "s"){
+        client.commands.get("skip").execute(message);
     }
     
 })
